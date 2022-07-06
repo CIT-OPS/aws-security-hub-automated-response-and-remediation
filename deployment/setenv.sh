d16912c47d1e11f6fd89c615c2131fafd968f922
EC2 Default User <ec2-user@ip-10-250-10-66.ec2.internal>
2022-07-06 13:27:46 +0000

    updated s3.9

--- deployment/setenv.sh
+++ deployment/setenv.sh
@@ -1,3 +1,3 @@
 export DIST_OUTPUT_BUCKET=sharr-deploy
-export DIST_VERSION=v1.5.0.cnxc.ken
+export DIST_VERSION=v1.5.0.cnxc.3
 export DIST_SOLUTION_NAME=aws-security-hub-automated-response-and-remediation
