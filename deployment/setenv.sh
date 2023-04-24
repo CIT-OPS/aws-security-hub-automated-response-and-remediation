eb1fd4bc9825e47105243b3933d159e149222aaa
Ken Papagno <kenneth.papagno@concentrix.com>
2023-04-24 09:13:07 -0400

    Push up

--- deployment/setenv.sh
+++ deployment/setenv.sh
@@ -1,4 +1,3 @@
 export DIST_OUTPUT_BUCKET=sharr-deploy
-export DIST_VERSION=v2.0.cnxc.1
-
+export DIST_VERSION=v2.0.1.cnxc
 export DIST_SOLUTION_NAME=aws-security-hub-automated-response-and-remediation
